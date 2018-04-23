d775ef451b7b6e86a3e3c01d287c8bdd12b45664
coder <coder@YARE.local>
2018-04-24 00:18:01 +0800

    create table category product

--- shopapp/models.py
+++ shopapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, null=False)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20, null=False)
+    price = models.IntegerField(null=False)
+    quantity = models.IntegerField(null=False)
+    description = models.TextField(default="No description")
+    img = models.ImageField(upload_to='shopapp/', null=False)
+
+    def __str__(self):
+        return self.name
