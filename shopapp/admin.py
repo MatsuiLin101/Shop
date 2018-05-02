67fc261b7d232c8934f702954842bcff75d8505c
coder <coder@YARE.local>
2018-05-02 10:09:06 +0800

    Order OrderItem models

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -10,6 +10,11 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('oid', 'name', 'status', 'order_date')
+
 
 admin.site.register(models.Category, CategoryAdmin)
 admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.OrderItem)
--- shopapp/models.py
+++ shopapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -18,3 +19,45 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+
+    PAYCARD = "CARD"
+    PAYPOD = "POD"
+    PAY_CHOICES = {
+        (PAYCARD, "PayCard"),
+        (PAYPOD, "PayPOD"),
+    }
+
+    PENDING = 1
+    PROCESSING = 2
+    ONTHEWAY = 3
+    DELIVERED = 4
+    STATUS_CHOICES = {
+        (PENDING, "Pending"),
+        (PROCESSING, "Processing"),
+        (ONTHEWAY, "On the Way"),
+        (DELIVERED, "Delivered"),
+    }
+
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    oid = models.CharField(max_length=14, null=False)
+    order_name = models.CharField(max_length=20, null=False)
+    order_tel = models.PositiveIntegerField()
+    order_address = models.CharField(max_length=100, null=False)
+    pay = models.CharField(max_length=10, choices=PAY_CHOICES)
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    order_date = models.DateTimeField(auto_now_add=True)
+    finish_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.oid
+
+class OrderItem(models.Model):
+    oid = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.oid.oid
