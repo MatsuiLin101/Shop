264f796f2067ed41ea1f792ea18b931357ba9952
coder <coder@YARE.local>
2018-05-07 20:11:41 +0800

    paypal notification finish

--- shopapp/paypal_signal.py
+++ shopapp/paypal_signal.py
@@ -1,10 +1,12 @@
 from . import models
-from paypal.standard.models import ST_PP_COMPLETED
+from paypal.standard.models import ST_PP_COMPLETED, ST_PP_PENDING
 from paypal.standard.ipn.signals import valid_ipn_received
 
 def payment_notification(sender, **kwargs):
     ipn_obj = sender
-    if ipn_obj.payment_status == ST_PP_COMPLETED:
+    # paypal post payment_type is Pending
+#    if ipn_obj.payment_status == ST_PP_COMPLETED:
+    if ipn_obj.payment_status == ST_PP_PENDING:
         order_id = ipn_obj.invoice.split("-")[-1]
         order = models.Order.objects.get(oid=order_id)
         order.status = 2
